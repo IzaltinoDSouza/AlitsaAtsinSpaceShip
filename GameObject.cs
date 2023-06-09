c0e011fe364101c4d42b79acabba14d1b51458c5
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-09 03:00:44 -0400

    Add X,Y in GameObject
    This make possible to change the position of object without create new vector every time to set X or Y,
    this is hidden behide of scenes

--- GameObject.cs
+++ GameObject.cs
@@ -7,7 +7,15 @@ namespace AASS
     public abstract class GameObject
     {
         public Vector2 Position{get;set;}
+
+        public float X { get {return Position.X;}
+                         set {Position = new Vector2(value,Position.Y); }
+                       }
+        public float Y { get {return Position.Y;}
+                         set {Position = new Vector2(Position.X,value); }
+                       }
         public bool IsActive{get;set;}
+
         public abstract void Initialize();
         public abstract void Update(GameTime gameTime);
         public abstract void Draw(SpriteBatch spriteBatch);
