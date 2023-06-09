a9216bfda35e996b11f44772057899c7a8be0d4c
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-09 01:36:01 -0400

    Create a GameObject class to be blueprint for others objects in the game

---
+++ GameObject.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AASS
+{
+    public abstract class GameObject
+    {
+        public Vector2 Position{get;set;}
+        public bool Visible{get;set;}
+        public abstract void Initialize();
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
