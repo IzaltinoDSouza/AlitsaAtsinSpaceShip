d0de417e7eaecd268a97cc305648a26302ac187b
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-09 21:33:25 -0400

    Add Atsin Spaceship to be 2º player
    Now that InputHandler,MoveUp/DownCommand and IMovementVertical make easy
    to bind keys and execute command to move up and down the two spaceship.

--- Universe.cs
+++ Universe.cs
@@ -11,21 +11,33 @@ namespace AASS
     {
         private List<GameObject> _gameObjects;
         private SpaceShip _alitsa;
+        private SpaceShip _atsin;
         private InputHandler _input;
 
         public void Initialize()
         {
+            _input = new InputHandler();
+            
             _gameObjects = new List<GameObject>();
-
-           _alitsa = new SpaceShip(new Rectangle(120,604,104,84),
+            
+            //Alitsa
+            _alitsa = new SpaceShip(new Rectangle(120,604,104,84),
                                    new Vector2(104/2,84/2));
 
-            _input = new InputHandler();
             _input.Bind(Keys.W,new MoveUpCommand(_alitsa));
             _input.Bind(Keys.S,new MoveDownCommand(_alitsa));
-
+            
             _gameObjects.Add(_alitsa);
-
+            
+            //Atsin
+            _atsin = new SpaceShip(new Rectangle(518,493,82,84),
+                                   new Vector2(104/2,360));
+            
+            _input.Bind(Keys.Up,new MoveUpCommand(_atsin));
+            _input.Bind(Keys.Down,new MoveDownCommand(_atsin));
+            
+            _gameObjects.Add(_atsin);
+            
             foreach(var gameObject in _gameObjects)
             {
                 gameObject.Initialize();
