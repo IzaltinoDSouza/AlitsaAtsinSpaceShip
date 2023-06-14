cc6e9837f0ece9c824b5d9aee1b894ca2704999f
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-14 01:44:28 -0400

    Add IBoxCollision and IHealth to this class

--- Meteor.cs
+++ Meteor.cs
@@ -5,10 +5,13 @@ using AASS.AtlasTexture;
 
 namespace AASS
 {
-    public class Meteor : GameObject
+    public class Meteor : GameObject,IHealth,IBoxCollision
     {
         private Rectangle _atlasMeteor;
         private float _speed;
+        public Rectangle Shape {get;set;}
+        public int MaxHealth {get;set;}
+        public int CurrentHealth {get;set;}
 
         public Meteor(Rectangle atlasMeteor,
                       Vector2 position,
@@ -20,11 +23,14 @@ namespace AASS
         }
         public override void Initialize()
         {
-           //Nothing
+            Shape = new Rectangle((int)X,(int)Y,_atlasMeteor.Width,_atlasMeteor.Height);
+            CurrentHealth = 100;
+            MaxHealth = 100;
         }
 
         public override void Update(GameTime gameTime)
         {
+            Shape = new Rectangle((int)X,(int)Y,_atlasMeteor.Width,_atlasMeteor.Height);
             float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             X -= _speed * elapsedTime;
         }
@@ -34,6 +40,19 @@ namespace AASS
                                     new AtlasTexture2D(0,_atlasMeteor),
                                                        Position);
         
+        }
+        public void TakeDamage(int amount)
+        {
+            CurrentHealth -= amount;
+            if(CurrentHealth < 0)
+            {
+                CurrentHealth = 0;
+                IsActive = false;
+            }
+        }
+        public void Heal(int amount)
+        {
+            //Nothing
         }
     }
 }
