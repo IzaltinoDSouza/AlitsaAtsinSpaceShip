c56283d4d75507d9e85f5ba5f2ec5484e3a02eee
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-14 20:31:43 -0400

    Add CollisionDebug and meteors objects to test the collision handler

--- Universe.cs
+++ Universe.cs
@@ -17,18 +17,25 @@ namespace AASS
         private SpaceShip _atsin;
         private InputHandler _input;
         private CollisionHandler _collision;
+        private CollisionDebug   _collisionDebug;
 
+        private Random _random;
+        private float _createMeteorCountdown;
         public void Initialize()
         {
+            _random = new Random();
+            _createMeteorCountdown = 10f;
+
             _input = new InputHandler();
             _collision = new CollisionHandler();
+            _collisionDebug = new CollisionDebug();
             
             _gameObjects = new Dictionary<string,List<GameObject>>();
             
             //Alitsa
             _alitsa = new SpaceShip("Alitsa",new Rectangle(120,604,104,84),
                                     		 new Vector2(128,84/2));
-            
+
             var _alitsaChildrens = new List<GameObject>();
             _alitsaChildrens.Add(_alitsa);
 
@@ -36,20 +43,32 @@ namespace AASS
             _input.Bind(Keys.S,new MoveDownCommand(_alitsa));
 
             _gameObjects.Add(_alitsa.Name,_alitsaChildrens);
-            
+
             //Atsin
             _atsin = new SpaceShip("Atsin",new Rectangle(518,493,82,84),
                                    		   new Vector2(128,Global.ScreenHeight-(84/2)));
-            
+
              var _atsinChildrens = new List<GameObject>();
             _atsinChildrens.Add(_atsin);
 
             _input.Bind(Keys.Up,new MoveUpCommand(_atsin));
             _input.Bind(Keys.Down,new MoveDownCommand(_atsin));
-            
+
             _gameObjects.Add(_atsin.Name,_atsinChildrens);
+
+            //_collision.WhenCollide("Alitsa","Atsin",new IHealthAction("Atsin",-1));
+
+            var MeteorAndAlitsaCollideActions = new List<ICollisionAction>();
+            MeteorAndAlitsaCollideActions.Add(new IHealthAction(_alitsa.Name,-1));
+            MeteorAndAlitsaCollideActions.Add(new IHealthAction("Meteor",-100));
+            _collision.WhenCollide("Meteor",_alitsa.Name,MeteorAndAlitsaCollideActions);
+
+            var MeteorAndAtsinCollideActions = new List<ICollisionAction>();
+            MeteorAndAtsinCollideActions.Add(new IHealthAction(_atsin.Name,-1));
+            MeteorAndAtsinCollideActions.Add(new IHealthAction("Meteor",-100));
+            _collision.WhenCollide("Meteor",_atsin.Name,MeteorAndAtsinCollideActions);
             
-            _collision.WhenCollide("Alitsa","Atsin",new IHealthAction("Atsin",-1));
+            _gameObjects.Add("Meteor",new List<GameObject>());
             
             foreach(var gameObjects in _gameObjects)
             {
@@ -85,15 +104,27 @@ namespace AASS
             }
             
             _collision.HandleCollision(_gameObjects);
+            _createMeteorCountdown -= elapsedTime;
+            if(_createMeteorCountdown <= 0.0)
+            {
+                Rectangle meteorVariation = _random.Next(0,1) == 0 ? new Rectangle(346,814,18,18) :
+                                                                     new Rectangle(399,814,16,15);
+                var meteorRandomPosition =
+                    new Vector2(_random.Next(((int)Global.ScreenWidth-20)/2,(int)Global.ScreenWidth-20),
+                                _random.Next(20,(int)Global.ScreenHeight-20));
+                 _gameObjects["Meteor"].Add(new Meteor("Meteor",meteorVariation,meteorRandomPosition,75f));
+                _createMeteorCountdown = 0.8f;
+            }
         }
-        public void Draw(SpriteBatch sprite)
+
+        public void Draw(SpriteBatch spriteBatch)
         {
             //Background
             for(int y = (int)_backgroundPosition.Y;y < Global.ScreenHeight;y += _backgroundTexture.Height)
             {
                 for(int x = (int)_backgroundPosition.X;x < Global.ScreenWidth;x += _backgroundTexture.Width)
                 {
-                    sprite.Draw(_backgroundTexture,new Vector2(x,y),Color.White);
+					spriteBatch.Draw(_backgroundTexture,new Vector2(x,y),Color.White);
                 }
             }
             //Draw objects
@@ -101,7 +132,11 @@ namespace AASS
             {
             	foreach(var gameObject in gameObjects.Value)
             	{
-                	if(gameObject.IsActive) gameObject.Draw(sprite);
+                    if(gameObject.IsActive)
+                    {
+                        gameObject.Draw(spriteBatch);
+                        _collisionDebug.Draw(spriteBatch,gameObject);
+                    }
                 }
             }
         }
